b0853547dc0c4f8b6efbaca1094ec2b86f3c820d
sybui2004 <symerline2004@gmail.com>
2026-04-15 12:40:40 +0700

    fix(olap): Add dim location for cube2

--- dags/transform/olap/cube2_time_product_location.py
+++ dags/transform/olap/cube2_time_product_location.py
@@ -1,11 +1,10 @@
-# Cuboid: olap_inv_base
+# Cuboid: olap_inv_time_product_location
 EXTRACT_SQL = """\
 SELECT
     dt.year,
     dt.quarter,
     dt.month,
     fi.product_key,
-    fi.store_key,
     dl.state,
     dl.city,
     SUM(fi.quantity_on_hand) AS total_quantity_on_hand
@@ -13,8 +12,7 @@ FROM dwh.Fact_Inventory fi
 JOIN dwh.Dim_Time dt ON fi.time_key = dt.time_key
 JOIN dwh.Dim_Store ds ON fi.store_key = ds.store_key
 JOIN dwh.Dim_Location dl ON ds.location_key = dl.location_key
-GROUP BY dt.year, dt.quarter, dt.month, fi.product_key, fi.store_key, dl.state, dl.city
+GROUP BY dt.year, dt.quarter, dt.month, fi.product_key, dl.state, dl.city
 """
-TARGET_COLUMNS = ['year', 'quarter', 'month', 'product_key', 'store_key', 'state', 'city', 'total_quantity_on_hand']
-TARGET_TABLE   = 'olap_inv_base'
-
+TARGET_COLUMNS = ['year', 'quarter', 'month', 'product_key', 'state', 'city', 'total_quantity_on_hand']
+TARGET_TABLE   = 'olap_inv_time_product_location'
